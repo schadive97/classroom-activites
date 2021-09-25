6580f3581e7e207f1258e5fc33f510982f3bf59e
schadive97 <schadive@vt.edu>
2021-09-25 10:27:05 -0400

    19.3

--- 19.2-activities/03_todo_app/unsolved/js/index.js
+++ 19.2-activities/03_todo_app/unsolved/js/index.js
@@ -1,5 +1,31 @@
 // Create event listener to enter items below
+("#enter").on("click", fuction() {
+
+
+    var task=$("#todoItem").val();
+    $("#todoList").append("<div class='task'>" +task +"</div class='x fas fa-times'></div></div>");
+
+
+});
 
 // Create event listener to delete items below
+$(document).on("click", ".x", function(){
 
-// Create event listener to toggle 
+    $(this).parent().remove();
+}
+// Create event listener to toggle
+
+$(document).on("click",".task", function(){
+
+    if ($(this).hasClass ("done"))
+        $(this).find("div").removeClass("fa-times");
+        $(this).find("div").addClass("fa-socks");
+
+    }
+
+    else {
+        $(this).find("div").addClass("fa-times");
+
+    }
+
+});
